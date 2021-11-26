f06f17e754af399a4c4d6eff9b61f6ed85edfc53
syatibiafnan <syatibiafnan@gmail.com>
2021-11-26 22:25:07 +0800

    ICT602 - Lab 2b - List View

---
+++ app/src/main/java/com/example/lab2b_listview/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.lab2b_listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        String[]    values = new String[]{
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, android.R.id.text1, values);
+        listView.setAdapter(adapter);
+
+    }
+}
